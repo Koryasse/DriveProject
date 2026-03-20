8853f4b758bb4dcbe5fe83c2cc7cae3eb6cf84e1
koryasse <korbi_yassine@outlook.com>
2026-03-20 10:12:09 +0000

    Modification du venv/

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/yassine/Bureau/DriveProject1.0/backend/venv
+set -gx VIRTUAL_ENV /home/yassine/DriveProject/backend/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
