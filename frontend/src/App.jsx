162fb00a05dea061ceaef49c2e53be069c03ec4f
koryasse <korbi_yassine@outlook.com>
2026-04-24 10:17:59 +0200

    Correction erreur importation de Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import Home from './pages/Home/Home.jsx'
 import Drive from './pages/Drive/Drive.jsx'
 import Login from './pages/Login/Login.jsx'
 import Register from './pages/Register/Register.jsx'
